7d6231d76cf76c393d610a4a788f42f9ac669cb8
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-01-14 19:32:22 +0700

    init tailwind and flowbite

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export const content = [
+  "./resources/**/*.blade.php",
+  "./resources/**/*.js",
+  "./resources/**/*.vue",
+  "./node_modules/flowbite/**/*.js"
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [
+  require('flowbite/plugin')
+];
